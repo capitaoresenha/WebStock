1bf89ac07c2774e8c8988bb78b07d634c560d5b0
Eduardo Henrique <jiinxzkk.lhp.zo@gmail.com>
2025-05-20 19:54:42 -0300

    mensagem

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    alert('Obrigado pelo envio!\nSeus dados foram enviados com sucesso.')
+    location.reload();
+});
